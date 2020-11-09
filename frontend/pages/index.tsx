aee9741a2ea3ec8b2e5b518aa37129cdb468d13d
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2020-11-09 19:45:08 +0100

    Add dark theme

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import DashBoard from "../components/Dashboard";
 import Navigation from "../components/Navigation";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Home() {
   return (
