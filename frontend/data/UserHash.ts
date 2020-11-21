dc04a61bee2e64c565f62dad972d860a85bfda7a
aboft <57803455+aboft@users.noreply.github.com>
2020-11-21 14:06:03 -0500

    Update frontend/data/UserHash.ts
    Co-authored-by: Alex Macleod <alex@macleod.io>

--- frontend/data/UserHash.ts
+++ frontend/data/UserHash.ts
@@ -1,12 +1,9 @@
 export default function userHash(str: string): number {
-  let hash: number = 0;
-  let i: number;
-  let char: number;
-  if (str.length == 0) return hash;
-  for (i = 0; i < str.length; i++) {
-    char = str.charCodeAt(i);
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    const char = str.charCodeAt(i);
     hash = (hash << 5) - hash + char;
-    hash = hash & hash;
+    hash |= 0;
   }
   return hash;
 }
