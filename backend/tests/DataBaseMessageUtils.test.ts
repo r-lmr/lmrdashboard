0e4958565b52486446b1547a65463ed7172791bf
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2020-12-15 21:42:52 +0100

    Working mocked db

--- backend/tests/DataBaseMessageUtils.test.ts
+++ backend/tests/DataBaseMessageUtils.test.ts
@@ -1,7 +1,37 @@
-import { DatabaseMessageUtils, IMessage } from '../irc/utils/db/Messages';
+import knex from 'knex';
+import mockDb from 'mock-knex';
+import { DatabaseMessageUtils } from '../irc/utils/db/Messages';
+const db = knex({
+  client: 'mysql',
+});
 
-test('foo', async () => {
-  const messages: IMessage[] = await DatabaseMessageUtils.getLinesLastNDays(7);
-  console.log(messages);
-  expect(messages.length).toBeGreaterThan(0)
+mockDb.mock(db);
+
+const MOCKED_DATA = [
+  {
+    user: 'foo',
+    server: 'bar',
+    message: 'foobar',
+    dateCreated: new Date()
+  },
+  {
+    user: 'asdf',
+    server: 'fdsa',
+    message: 'qwer',
+    dateCreated: new Date()
+  }
+]
+
+const tracker = require('mock-knex').getTracker();
+tracker.install();
+tracker.on('query', (query: { response: (arg0: any) => void; }) => {
+  query.response(MOCKED_DATA);
+});
+
+describe('foo', () => {
+  it('bar', async () => {
+    const res = await DatabaseMessageUtils.getLinesLastNDays(1);
+    const users = res;
+    console.log(users);
+  });
 });
