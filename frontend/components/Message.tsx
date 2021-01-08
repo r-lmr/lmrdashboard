07f68e6aa7b5a824709c29b0d8ed17e5e5bc4ba9
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2021-01-08 14:04:40 +0100

    Parse for ACTION, transform message accordingly

--- frontend/components/Message.tsx
+++ frontend/components/Message.tsx
@@ -1,9 +1,22 @@
 import userHash from '../data/UserHash';
 
 export default function Message(props: IMessage) {
+
+  /**
+   * Parses for ACTION, which is sent when /me is used
+   * @returns the message with either a colon or nothing prefixed
+   */
+  function formatMessage(): string {
+    // Slice is needed to ignore an unrenderable character in front of ACTION
+    if (props.message.slice(1).startsWith('ACTION')) {
+      return props.message.replace('ACTION', '');
+    }
+    return `: ${props.message}`;
+  }
+
   return (
     <div>
-      [{props.dateCreated}] <span className={`nick-${userHash(props.nick) % 16}`}>{props.nick}</span>: {props.message}
+      [{props.dateCreated}] <span className={`nick-${userHash(props.nick) % 16}`}>{props.nick}</span>{formatMessage()}
     </div>
   );
 }
