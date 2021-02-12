4a09b43b76f068fb409deb7dc3ea80c52d46e237
Max Audron <audron@cocaine.farm>
2021-02-12 23:43:12 +0100

    backend: fix init db migration
    had to add await to the functions for them to execute and remove columns
    that are added in migrations

--- backend/database/knex_migrations/20210101000000_init_db.ts
+++ backend/database/knex_migrations/20210101000000_init_db.ts
@@ -1,27 +1,25 @@
 import * as Knex from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  knex.schema.createTable('friend_scores', function (table) {
+  await knex.schema.createTable('friend_scores', function (table) {
     table.string('user').unique();
     table.integer('duccs');
   });
-  knex.schema.createTable('killer_scores', function (table) {
+  await knex.schema.createTable('killer_scores', function (table) {
     table.string('user').unique();
     table.integer('duccs');
   });
-  knex.schema.createTable('last_messages', function (table) {
+  await knex.schema.createTable('last_messages', function (table) {
     table.string('user');
     table.string('server');
     table.string('message', 512);
     table.dateTime('dateCreated').defaultTo(knex.fn.now());
-    table.boolean('userIsBot');
   });
-  knex.schema.createTable('line_counts', function (table) {
+  await knex.schema.createTable('line_counts', function (table) {
     table.integer('count');
-    table.integer('botLines');
     table.dateTime('date');
   });
-  knex.schema.createTable('online_users', function (table) {
+  await knex.schema.createTable('online_users', function (table) {
     table.string('user').unique();
     table.string('role');
     table.dateTime('dateCreated').defaultTo(knex.fn.now());
@@ -29,9 +27,9 @@ export async function up(knex: Knex): Promise<void> {
 }
 
 export async function down(knex: Knex): Promise<void> {
-  knex.schema.dropTable('friend_scroes');
-  knex.schema.dropTable('killer_scroes');
-  knex.schema.dropTable('last_messages');
-  knex.schema.dropTable('line_counts');
-  knex.schema.dropTable('online_users');
+  await knex.schema.dropTable('friend_scores');
+  await knex.schema.dropTable('killer_scores');
+  await knex.schema.dropTable('last_messages');
+  await knex.schema.dropTable('line_counts');
+  await knex.schema.dropTable('online_users');
 }
