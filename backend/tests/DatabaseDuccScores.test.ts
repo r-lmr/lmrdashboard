caf911e0e1912c24c2eb3e6394ef7739d6e13ede
aboft <coltonmmarkle@gmail.com>
2021-02-15 16:16:08 -0500

    fix linting issues

--- backend/tests/DatabaseDuccScores.test.ts
+++ backend/tests/DatabaseDuccScores.test.ts
@@ -9,8 +9,10 @@ const mockJoinConfig: JoinConfig = {
 };
 
 test('parse scores correctly based off of friend or killer', async () => {
-  const mockFriendScoresMsg = `:gonzobot!gonzo@snoonet/bot/gonzobot PRIVMSG #aboftytest :(aboft) Duck friend scores in #aboftytest: a​bofty: 1,777 • a​boft: 13213 • Asmodean: 6,9,49`;
-  const mockKillerScoresMsg = `:gonzobot!gonzo@snoonet/bot/gonzobot PRIVMSG #aboftytest :(aboft) Duck killer scores in #aboftytest: a​smodean: 1 • a​boft: 13213 • Asmodean: 6,9,49`;
+  const mockFriendScoresMsg =
+    ':gonzobot!gonzo@snoonet/bot/gonzobot PRIVMSG #aboftytest :(aboft) Duck friend scores in #aboftytest: a​bofty: 1,777 • a​boft: 13213 • Asmodean: 6,9,49';
+  const mockKillerScoresMsg =
+    ':gonzobot!gonzo@snoonet/bot/gonzobot PRIVMSG #aboftytest :(aboft) Duck killer scores in #aboftytest: a​smodean: 1 • a​boft: 13213 • Asmodean: 6,9,49';
   const ircMessageProcessor = await IrcMessageProcessor.Instance(null!, mockJoinConfig);
   const mockFriendScores = await ircMessageProcessor.parseMessage(mockFriendScoresMsg);
   const mockKillerScores = await ircMessageProcessor.parseMessage(mockKillerScoresMsg);
@@ -21,7 +23,6 @@ test('parse scores correctly based off of friend or killer', async () => {
 test('insert or update ducc scores', async () => {
   const friendScore = ['aboft: 1234', 'asmodean: 1,324,23'];
   const killerScore = ['audron: 5,443', 'alexendoo: 44'];
-
   await DatabaseDuccUtils.insertOrUpdateDuccScores(friendScore, 'friend');
   await DatabaseDuccUtils.insertOrUpdateDuccScores(killerScore, 'killer');
 });
