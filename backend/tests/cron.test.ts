3c06256705cee38b8cf216285bf545d991589a3f
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2021-03-31 12:01:34 +0200

    Pass time string in the format of hh:mm to scheduleDailyEvent

--- backend/tests/cron.test.ts
+++ backend/tests/cron.test.ts
@@ -1,17 +1,27 @@
 import { scheduleDailyEvent } from "../utils/cron";
 
 describe('cron scheduling daily events (only the first trigger is checked)', () => {
+  /**
+   * @returns a string in the format of hh:mm, as needed by scheduleDailyEvent
+   */
+  function getTimeString(date: Date): string {
+    const hours = date.getHours();
+    const hoursString = ('0' + hours).slice(-2);
+    const minutes = date.getMinutes();
+    const minutesString = ('0' + minutes).slice(-2);
+    return `${hoursString}:${minutesString}`
+  }
+
   test('trigger function is called at the specified time and day', async () => {
     const mockCallback = jest.fn(() => console.log('callback called'));
 
     const date = new Date();
-    console.log('time passed to scheduleDailyEvent', `${date.getHours()}:${date.getMinutes()}`)
     scheduleDailyEvent(
-      `${date.getHours()}:${date.getMinutes()}`,
+      getTimeString(date),
       mockCallback,
       date.getDay());
 
-    await new Promise((r) => setTimeout(r, 5000));
+    await new Promise((r) => setTimeout(r, 1000));
     expect(mockCallback.mock.calls.length).toBe(1);
   });
 
@@ -20,11 +30,11 @@ describe('cron scheduling daily events (only the first trigger is checked)', () 
 
     const date = new Date();
     scheduleDailyEvent(
-      `${date.getHours()}:${date.getMinutes()}`,
+      getTimeString(date),
       mockCallback,
       date.getDay() + 1);
 
-    await new Promise((r) => setTimeout(r, 5000));
+    await new Promise((r) => setTimeout(r, 1000));
     expect(mockCallback.mock.calls.length).toBe(0);
   });
 });
