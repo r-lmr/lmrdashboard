a3a33a3c119c77a227aa1d1c5602d203f3fe3be8
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2021-04-07 20:01:58 +0200

    Make terminals collapsible

--- frontend/components/Terminal.tsx
+++ frontend/components/Terminal.tsx
@@ -1,15 +1,20 @@
+import { useState } from 'react';
 import { Container, Row } from 'reactstrap';
 import TerminalBar from './TerminalBar';
 
 export default function Terminal(props: IProps): JSX.Element {
+  const [containerVisible, setContainerVisible] = useState<boolean>(true);
+
+  const onTerminalButtonClick = () => {
+    setContainerVisible(!containerVisible);
+  };
+
   return (
     <>
-      <TerminalBar title={'Online Users'} />
-      <div className={'terminal-container'}>
-        <Container className='terminal-container-inner' style={props.containerStyle}>
-          <Row style={props.rowStyle}>
-            {props.children}
-          </Row>
+      <TerminalBar title={'Online Users'} onButtonClick={onTerminalButtonClick} />
+      <div className={'terminal-container'} style={{ display: containerVisible ? '' : 'none' }}>
+        <Container className="terminal-container-inner" style={props.containerStyle}>
+          <Row style={props.rowStyle}>{props.children}</Row>
         </Container>
       </div>
     </>
--- frontend/components/TerminalBar.tsx
+++ frontend/components/TerminalBar.tsx
@@ -2,9 +2,9 @@ export default function TerminalBar(props: IProps): JSX.Element {
   return (
       <div className={'terminal-bar'}>
         <div className={'terminal-buttons'}>
-          <div className={'terminal-button exit'}></div>
+          <div className={'terminal-button exit'} onClick={props.onButtonClick}></div>
           <div className={'terminal-button maximize'}></div>
-          <div className={'terminal-button minimize'}></div>
+          <div className={'terminal-button minimize'} onClick={props.onButtonClick}></div>
         </div>
         {props.title || 'irc - #linuxmasterrace'}
       </div>
@@ -13,4 +13,5 @@ export default function TerminalBar(props: IProps): JSX.Element {
 
 interface IProps {
   title?: string;
+  onButtonClick: () => void;
 }
