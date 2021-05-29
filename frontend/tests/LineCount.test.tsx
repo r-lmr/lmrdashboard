8ff73264440ee8a6a9b6c6fd0da0f485de8a7122
Lorenz Leitner <lrnz.ltnr@gmail.com>
2021-05-29 14:55:34 +0200

    Fix percentage rounding for <10 values

--- frontend/tests/LineCount.test.tsx
+++ frontend/tests/LineCount.test.tsx
@@ -3,6 +3,21 @@ import renderer from 'react-test-renderer';
 import { expect } from '@jest/globals';
 import LineCount from '../components/LineCount';
 
+test('LineCount Component has <10% bot messages', () => {
+  const component = renderer.create(
+    <LineCount
+      key={'key'}
+      lineCount={1000}
+      botLines={55}
+      date={'2021-01-01'}
+      message={'foo'}
+    />,
+  );
+
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
 test('LineCount Component has 25% bot messages', () => {
   const component = renderer.create(
     <LineCount
