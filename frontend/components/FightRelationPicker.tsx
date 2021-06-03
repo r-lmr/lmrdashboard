8bd8ec04565d0b73b909227117e13f4ecb4c1331
Lorenz Leitner <lrnz.ltnr@gmail.com>
2021-06-03 14:27:14 +0200

    Add styles

--- frontend/components/FightRelationPicker.tsx
+++ frontend/components/FightRelationPicker.tsx
@@ -2,11 +2,15 @@ import { useState } from 'react';
 import { InputGroup, InputGroupAddon, Input, Button } from 'reactstrap';
 import { getEventSourceBaseUrl } from '../data/EventSource';
 import { FaSearch } from 'react-icons/fa';
+import { getNickCSSClass } from '../data/UserHash';
 
 export default function FightRelationPicker(): JSX.Element {
   const [nick1, setNick1] = useState<string>('');
   const [nick2, setNick2] = useState<string>('');
+  const [nick1Wins, setNick1Wins] = useState<number>(-1);
+  const [nick2Wins, setNick2Wins] = useState<number>(-1);
   const [displayError, setDisplayError] = useState<boolean>(false);
+  const [resultsAvailable, setResultsAvailable] = useState<boolean>(false);
 
   const handleSearchRequest = () => {
     if (nick1.length === 0 || nick2.length === 0) {
@@ -24,10 +28,13 @@ export default function FightRelationPicker(): JSX.Element {
         }
       })
       .then((data) => {
-        console.log(data);
+        setNick1Wins(data.nick1Wins);
+        setNick2Wins(data.nick2Wins);
+        setResultsAvailable(true);
       })
       .catch((e) => {
         console.error(e);
+        setResultsAvailable(false);
         setDisplayError(true);
         setTimeout(() => {
           setDisplayError(false);
@@ -36,7 +43,7 @@ export default function FightRelationPicker(): JSX.Element {
   };
 
   return (
-    <div>
+    <div className={'fight-relation-picker-container'}>
       <InputGroup>
         <Input
           placeholder={'Nick 1'}
@@ -57,6 +64,19 @@ export default function FightRelationPicker(): JSX.Element {
         </InputGroupAddon>
       </InputGroup>
       {displayError && <div>No match found.</div>}
+      {resultsAvailable && (
+        <div className={'fight-relation-results'}>
+          <div>
+            {/*TODO: Move these replaces into the getNickCSSClass function itself*/}
+            <span className={getNickCSSClass(nick1.replace(/[^a-zA-Z0-9]/g, ''))}>{nick1}'s</span> wins vs{' '}
+            <span className={getNickCSSClass(nick2.replace(/[^a-zA-Z0-9]/g, ''))}>{nick2}</span>: <b>{nick1Wins}</b>
+          </div>
+          <div>
+            <span className={getNickCSSClass(nick2.replace(/[^a-zA-Z0-9]/g, ''))}>{nick2}'s</span> wins vs{' '}
+            <span className={getNickCSSClass(nick1.replace(/[^a-zA-Z0-9]/g, ''))}>{nick1}</span>: <b>{nick2Wins}</b>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
